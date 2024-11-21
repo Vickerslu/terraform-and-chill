d380ea0577e0c0307a2a65b45d54d05238e50320
vickerslu <vickerslu@gmail.com>
2024-11-21 23:37:53 +0000

    setup

---
+++ environments/dev/main.tf
@@ -0,0 +1,19 @@
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.10.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "terraform-state"
+    storage_account_name = "lukesterraformstate"
+    container_name       = "lukesterraformstate"
+    key                  = "dev/terraform.tfstate"
+  }
+}
